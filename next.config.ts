389a2656f77a006b24cfe02869f309d9588da94e
Crl0sDEV <sandrinocarlosmiguel@gmail.com>
2026-08-02 10:17:41 +0800

    update again to fix offline issue

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
-  turbopack: {},
+  // turbopack: {}, // Removed so next-pwa Webpack plugins can run in dev
 };
 
 export default withPWA(nextConfig);
